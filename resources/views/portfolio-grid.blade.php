fb70499af9c366cee982f58c8ce5680d7d9a4d3a
George Popovic <georgesmpopovic@gmail.com>
2022-05-22 14:26:32 -0400

    portfolio-grid

---
+++ resources/views/portfolio-grid.blade.php
@@ -0,0 +1,114 @@
+<section class="page-section bg-light" id="portfolio">
+    <div class="container">
+        <div class="row">
+            <!-- Modal 1 -->
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/insdonuts.PNG" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Insomnia Donuts</div>
+                        <div class="portfolio-caption-subheading text-muted">What are you craving?</div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal 2 -->
+            <div class="col-lg-4 col-sm-6 mb-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/quantumleap.PNG" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Quantum Leap</div>
+                        <div class="portfolio-caption-subheading text-muted">Your favorite vegan haunt.</div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal 3 -->
+            <div class="col-lg-4 col-sm-6 mb-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/tweetnalysis.PNG" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Tweetnalysis</div>
+                        <div class="portfolio-caption-subheading text-muted">What are your tweets saying about you?
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal 4 --> 
+            <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/oconn.PNG" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">O'Connor & Co.</div>
+                        <div class="portfolio-caption-subheading text-muted">Tea time in New York.</div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal 5 -->
+            <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/mojoenglish.PNG" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Mojo English</div>
+                        <div class="portfolio-caption-subheading text-muted">English made easy through a game the
+                            whole family can enjoy.</div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal 6 -->
+            <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/lrapi.PNG" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Laravel Restful API</div>
+                        <div class="portfolio-caption-subheading text-muted">A RESTful API built with PHP's Laravel
+                            framework.</div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal 7 -->
+            <div class="col-lg-4 col-sm-6 my-4">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal7">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/walmart.PNG" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Walmart Bot</div>
+                        <div class="portfolio-caption-subheading text-muted">An automated checkout bot.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
