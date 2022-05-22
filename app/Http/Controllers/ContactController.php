9581bf23ee6ac96c22061c3188dac899154baaf3
George Popovic <georgesmpopovic@gmail.com>
2022-05-22 14:16:05 -0400

    configure smtp later

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -20,24 +20,24 @@ public function index(Request $request){
         'message' => 'required'
     ]);
 
-    // \Mail::send('email', [
-    // 'name' => $request->get('name'),
-    // 'email' => $request->get('email'),
-    // 'comment' => $request->get('message') ],
-    // function ($message) {
-    //         $message->from('george');
-    //         $message->to('george', 'George Popovic')
-    //         ->subject('Your Website Contact Form');
-    // });
+    // // \Mail::send('email', [
+    // // 'name' => $request->get('name'),
+    // // 'email' => $request->get('email'),
+    // // 'comment' => $request->get('message') ],
+    // // function ($message) {
+    // //         $message->from('george');
+    // //         $message->to('george', 'George Popovic')
+    // //         ->subject('Your Website Contact Form');
+    // // });
 
-    $data = [
-        // 'message' => $request->input('message'),
-        // 'name' => $request->input('name'),
-        // 'phone' => $request->input('phone'),
-        // 'email' => $request->input('email')
-    ];
+    // $data = [
+    //     // 'message' => $request->input('message'),
+    //     // 'name' => $request->input('name'),
+    //     // 'phone' => $request->input('phone'),
+    //     // 'email' => $request->input('email')
+    // ];
 
-    \Mail::to('georgesmpopovic@gmail.com')->send(new Email($data));
+    // \Mail::to('georgesmpopovic@gmail.com')->send(new Email($data));
 
     return back()->with('success', 'Thanks for contacting me, I will get back to you soon!');
 
