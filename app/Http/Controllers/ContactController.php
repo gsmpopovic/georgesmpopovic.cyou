d9c18e7668eed24bfd036b448df9528945714899
George Popovic <georgesmpopovic@gmail.com>
2022-07-28 13:32:54 -0400

    edited email and contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Mail\Email as Email; 
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -20,24 +21,14 @@ public function index(Request $request){
         'message' => 'required'
     ]);
 
-    // // \Mail::send('email', [
-    // // 'name' => $request->get('name'),
-    // // 'email' => $request->get('email'),
-    // // 'comment' => $request->get('message') ],
-    // // function ($message) {
-    // //         $message->from('george');
-    // //         $message->to('george', 'George Popovic')
-    // //         ->subject('Your Website Contact Form');
-    // // });
+    $data = [
+        'message' => $request->input('message'),
+        'name' => $request->input('name'),
+        'phone' => $request->input('phone'),
+        'email' => $request->input('email')
+    ];
 
-    // $data = [
-    //     // 'message' => $request->input('message'),
-    //     // 'name' => $request->input('name'),
-    //     // 'phone' => $request->input('phone'),
-    //     // 'email' => $request->input('email')
-    // ];
-
-    // \Mail::to('georgesmpopovic@gmail.com')->send(new Email($data));
+    Mail::to('georgesmpopovic@gmail.com')->send(new Email($data));
 
     return back()->with('message', 'Thanks for contacting me, I will get back to you soon!');
 
--- app/Mail/Email.php
+++ app/Mail/Email.php
@@ -5,7 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class Email extends Mailable
 {
@@ -21,15 +20,21 @@ class Email extends Mailable
     public function build()
     {
         $address = 'georgesmpopovic@gmail.com';
-        $subject = '';//"New mail from: $this->data['name'] @ $this->data['email']";
+        $subject = ''; //"New mail from: $this->data['name'] @ $this->data['email']";
         $name = 'George Popovic';
 
         return $this->view('emails.index')
-                    ->from($address, $name)
-                    ->cc($address, $name)
-                    ->bcc($address, $name)
-                    ->replyTo($address, $name)
-                    ->subject($subject);
-                    // ->with([ 'message' => $this->data['message'] ]);
+            ->from($address, $name)
+            ->cc($address, $name)
+            ->bcc($address, $name)
+            ->replyTo($address, $name)
+            ->subject($subject)
+            ->with(
+                ['msg' => $this->data['message'],
+                    'name' => $this->data["name"],
+                    'phone' => $this->data["phone"],
+                    "email" => $this->data["email"],
+                ]
+            );
     }
 }
